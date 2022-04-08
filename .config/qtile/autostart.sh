cd8a89d594adbe48fe41a6d289f8954b1795d253
angel <reysi2003@gmail.com>
2022-04-08 12:52:07 -0400

    Posting a screenshot (test)

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -4,3 +4,4 @@ udo timedatectl set-ntp 1
 nitrogen --restore
 xinput set-prop 'Logitech G502 HERO Gaming Mouse' 'libinput Accel Speed' -0.4
 udiskie &
+flameshot &
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Dotfiles
 Just my dotfiles :/
+
+Here some cool screenshots:
+
+![My desktop](/Images/Screenshots/Destop screeshot.png "The Desktop")
+
